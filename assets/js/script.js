75a2c33fd85009d7b8d8f47ed0f4de3c69c4be94
Kevin <kevhoang97@gmail.com>
2024-06-24 19:00:00 -0400

    added js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,14 @@ const searchFormEl = document.querySelector('#search-form');
 const resultTextEl = document.querySelector('#result-text');
 const resultContentEl= document.querySelector('#result-content');
 
+function getParams() {
+    const searchParamsArr = document.location.search.split('&');
+    const query = searchParamsArr[0].split('=').pop();
+    const exclude = searchParamsArr[1].split('=').pop();
+    const api = searchParamsArr[2].split('=').pop();
 
-function handleSearchFormSubmit (event) {
-    event.preventDefault();
-
-    const searchInputVal = document.querySelector('#search-input').value;
-    const apiKey = 'f7ae7c2ad24eafb7ed39958def2a3a06';
-
-    if (!searchInputVal) {
-        console.error('You need a search input!');
-        return;
-    }
-
-    const queryString = `api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&appid=${apiKey}`;
-
-    location.assign(queryString);
-
-    console.log(queryString);
+    searchApi(query, exclude, api);
 }
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 function showWeather (weatherObj) {
     console.log(weatherObj);
@@ -33,5 +21,76 @@ function showWeather (weatherObj) {
     weatherBody.classList.add('card-body');
     weatherCard.append(weatherBody);
 
-    
+    const bodyContentEl = document.createElement('p');
+    bodyContentEl.innerHTML +=
+    `<strong>${weather.city}`, ` ` , `${weather.date}</strong> <br/>`
+
+    if (weatherObj.temp) {
+        bodyContentEl.innerHTML +=
+        `Temp: , ${weatherObj.temp} <br/>`;
+    }
+    if (weatherObj.wind) {
+        bodyContentEl.innerHTML +=
+        `Wind: , ${weatherObj.wind} <br/>`;
+    }
+
+    if (weatherObj.humidity) {
+        bodyContentEl.innerHTML +=
+        `Humidity: , ${weatherObj.humidity} <br/>`;
+    }
+    weatherBody.append(bodyContentEl);
+    resultContentEl.append(weatherCard);
 }
+
+function searchApi (query, exclude, api) {
+    let weatherQueryURL = `api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&exclude=current,minutely,hourly&appid=${apiKey}`
+    fetch (weatherQueryUrl)
+    .then (function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+
+        return response.json();
+    })
+
+    .then(function(locRes) {
+        resultTextEl.textContent = locRes.search.query;
+
+        if(!locRes.results.length) {
+            console.log('No results found');
+            resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+        }
+        else {
+            resultContentEl.textContent = '';
+            for (let i = 0; i < locRes.results.length; i++) {
+                showWeather(locRes.results[i]);
+            }
+        }
+    })
+    .catch(function (error) {
+        console.error(error);
+    })
+}
+
+function handleSearchFormSubmit (event) {
+    event.preventDefault();
+
+    const searchInputVal = document.querySelector('#search-input').value;
+    const apiKey = 'f7ae7c2ad24eafb7ed39958def2a3a06';
+    const queryString = `api.openweathermap.org/data/2.5/forecast?q=${searchInputVal}&exclude=current,minutely,hourly&appid=${apiKey}`;
+
+    if (!searchInputVal) {
+        console.error('You need a search input!');
+        return;
+    }
+
+    
+    getParams();
+    location.assign(queryString);
+    console.log(searchInputVal);
+    console.log(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
